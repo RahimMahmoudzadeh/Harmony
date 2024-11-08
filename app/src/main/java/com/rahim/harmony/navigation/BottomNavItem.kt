4bd3198da99e8f4410512f01471c954f4672d6dd
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2024-11-08 19:06:23 +0330

    feat: app logo

--- app/src/main/java/com/rahim/harmony/navigation/BottomNavItem.kt
+++ app/src/main/java/com/rahim/harmony/navigation/BottomNavItem.kt
@@ -6,17 +6,21 @@ import com.rahim.harmony.R
 sealed class BottomNavItem(
     val route: String,
     @DrawableRes val iconNormal: Int?,
+    @DrawableRes val iconSelected: Int?,
 ) {
     data object Home : BottomNavItem(
         Destinations.Home.route,
         R.drawable.outline_home_24,
+        R.drawable.baseline_home_24,
     )
     data object Explore : BottomNavItem(
         Destinations.Explore.route,
         R.drawable.outline_explore_24,
+        R.drawable.baseline_explore_24,
     )
     data object Library : BottomNavItem(
         Destinations.Library.route,
         R.drawable.outline_library_music_24,
+        R.drawable.baseline_library_music_24,
     )
 }
